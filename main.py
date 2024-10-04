cd4f71d6bc9ba1511a182b6fdb53c57931d16ad5
Serban <serban.david@flowmatters.com>
2024-10-04 09:07:36 +0200

    done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 
 import argparse
 from dotenv import load_dotenv
@@ -14,17 +14,33 @@ parser.add_argument('--task', default='return numbers from 1 to 10')
 args = parser.parse_args()
 
 code_prompt = PromptTemplate(
-    template="Write a very short {language} funtion that will {task}",
-    input_variables=['language', 'task']
+    input_variables=['language', 'task'],
+    template="Write a very short {language} funtion that will {task}"
+)
+
+test_prompt = PromptTemplate(
+    input_variables=['language', 'code'],
+    template="Write a test for the following {language} code:\n{code}"
 )
 
 llm = OpenAI()
 
-code_chain = LLMChain(prompt=code_prompt, llm=llm)
+code_chain = LLMChain(prompt=code_prompt, llm=llm, output_key="code")
+test_chain = LLMChain(prompt=test_prompt, llm=llm, output_key='test')
 
-result = code_chain({
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=['language', 'task'],
+    output_variables=['code', 'test']
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
 
-print(result['text'])
+print('>>>>>>> GENERATED CODE:')
+print(result['code'])
+
+print('>>>>>>> GENERATED TEST:')
+print(result['test'])
